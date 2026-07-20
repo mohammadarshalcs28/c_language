25d75c2dc3d781aeee43725d4a266bb886f1f792
mohammadarshalcs28 <mohamma.arshal.cs28@iilm.edu>
2026-07-20 20:36:39 +0530

    new

---
+++ day8/Find the length of a string.c
@@ -0,0 +1,12 @@
+/*
+Find the length of a string.
+*/
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char st[]="hi hay 8 of learning dsa";
+    int length = strlen(st);
+    printf("the length of string:%d",length);
+    return 0;
+}
---
+++ day8/nput and print a string.c
@@ -0,0 +1,11 @@
+/*
+nput and print a string.
+*/
+#include<stdio.h>
+
+int main(){
+    char st[30];
+    gets(st);
+    printf("%s",st);
+    return 0;
+}
